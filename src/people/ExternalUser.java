02a7a9e6c5355b3a2c14db17ccc4e6f9bdf48b95
Matheus Reato <mathreatoeus@gmail.com>
2023-08-14 22:41:02 -0300

    Added InternalUser, ExternalUser and FacultyUser classes

---
+++ src/people/ExternalUser.java
@@ -0,0 +1,67 @@
+package people;
+
+import java.time.LocalDate;
+
+public class ExternalUser extends Person {
+    // Private Attributes ---------------------------------------------------------------
+    private LocalDate expirationDate;
+
+    // Constructor ----------------------------------------------------------------------
+    public ExternalUser(String name, String surname, String socialId, String username,
+                        String password) {
+        this.name = name;
+        this.surname = surname;
+        this.socialId = socialId;
+        this.username = username;
+        this.password = password;
+        this.isSuspended = false;
+        this.registrationDate = LocalDate.now();
+        this.memberId = generateId(this.username);
+        this.expirationDate = this.registrationDate.plusMonths(6);
+    }
+
+    // Getters --------------------------------------------------------------------------
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    // Setters --------------------------------------------------------------------------
+    private void setExpirationDate(LocalDate newExpirationDate) {
+        this.expirationDate = newExpirationDate;
+    }
+
+    // Methods --------------------------------------------------------------------------
+
+    /**
+     * Method to try and renew an ExternalUser's membership to the Library. It can only be
+     * renewed if the user is not suspended.
+     *
+     * @param amountOfTime The amount of time by which to extend the membership.
+     * @return true on success and false on failure.
+     */
+    public boolean renewMembership(long amountOfTime) {
+        if (!this.isSuspended) {
+            setExpirationDate(this.expirationDate.plusMonths(amountOfTime));
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Method to try and renew an ExternalUser's membership to the Library for 1
+     * semester (six months). It can only be renewed if the user is not suspended.
+     *
+     * @return true on success and false on failure.
+     */
+    public boolean renewMembership() {
+        if (!this.isSuspended) {
+            setExpirationDate(this.expirationDate.plusMonths(6));
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
--- src/people/Person.java
+++ src/people/Person.java
@@ -1,8 +1,12 @@
 package people;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+
 /**
- * Class that represents a general non-registered, non-staff person. Superclass of User
- * and Staff.
+ * Class that represents a general person. Superclass of InternalUser, FacultyUser,
+ * ExternalUser and Staff.
  *
  * @author Matheus Reato (RA: 244088), Caio Taishi (RA: 242908).
  */
@@ -12,6 +16,11 @@ public class Person {
     protected String name;
     protected String surname;
     protected String socialId;
+    protected String username;
+    protected String password;
+    protected boolean isSuspended;
+    protected LocalDate registrationDate;
+    protected String memberId;
 
     // Getters --------------------------------------------------------------------------
     public String getName() {
@@ -24,5 +33,98 @@ public class Person {
 
     protected String getSocialId() {
         return socialId;
+    }
+
+    protected String getUsername() {
+        return username;
+    }
+
+    protected String getPassword() {
+        return password;
+    }
+
+    protected boolean getIsSuspended() {
+        return isSuspended;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    // Setters --------------------------------------------------------------------------
+    protected void setUsername(String newUsername) {
+        this.username = newUsername;
+    }
+
+    protected void setPassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void setIsSuspended(boolean new_status) {
+        this.isSuspended = new_status;
+    }
+
+    // Methods --------------------------------------------------------------------------
+
+    /**
+     * Takes a username and hashes it using SHA-256 to generate a secure ID.
+     *
+     * @param username User's personal username.
+     * @return hash Member's ID.
+     */
+    protected String generateId(String username) {
+        try {
+            MessageDigest msgDigest = MessageDigest.getInstance(("SHA-256"));
+            byte[] hashBytes = msgDigest.digest(username.getBytes());
+
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : hashBytes) {
+                String hex = Integer.toHexString(0xff & b);
+
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return "none";
+        }
+    }
+
+    /**
+     * Method to try and change the current staff member's username upon authentication.
+     *
+     * @param newUsername New desired username.
+     * @param currentPassword This staff member's current password.
+     * @return true on success and false on failure.
+     */
+    public boolean changeUsername(String newUsername, String currentPassword) {
+        if (currentPassword.equals(this.password)) {
+            setUsername(newUsername);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Method to try and change the current staff member's password upon authentication.
+     *
+     * @param newPassword New desired password.
+     * @param currentPassword This staff member's current password.
+     * @return true on success and false on failure.
+     */
+    public boolean changePassword(String newPassword, String currentPassword) {
+        if (currentPassword.equals(this.password)) {
+            setPassword(newPassword);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
